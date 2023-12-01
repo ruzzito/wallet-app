6f13366f2661c35b3e210b785bba1d8fce70f9f8
ruzzito <facurodrilarrosa@gmail.com>
2023-12-01 17:45:53 -0300

    Mensaje descriptivo de los cambios

---
+++ src/app/pages/welcome/welcome.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.page.html',
+  styleUrls: ['./welcome.page.scss'],
+})
+export class WelcomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
